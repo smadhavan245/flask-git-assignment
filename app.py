304079a1666bc8e19a913de7926d37c430fd5e9e
Harshini B <your_email@gmail.com>
2026-02-05 17:32:58 +0530

    Add /api route and update JSON response

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-from flask import Flask
+from flask import Flask, jsonify
+import json
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-   return "Flask App Running from Tutedude branch"
+    return "Flask App Running from Tutedude branch"
+
+@app.route("/api")
+def api():
+    with open("data.json") as f:
+        return jsonify(json.load(f))
 
 if __name__ == "__main__":
     app.run(debug=True)
